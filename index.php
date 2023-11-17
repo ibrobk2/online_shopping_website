d2d2f8b61f686ba42975d198e9bd64ea6f6e3a02
Ibrahim Yusuf <ibrobk@gmail.com>
2023-11-17 08:43:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ONLINE SHOPPING WEBSITE</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <!-- Navbar -->
+        </header>
+        <main>
+            <!-- Main Contents -->
+            <h1>Welcome to Online Shopping Website</h1>
+        </main>
+
+        <footer>
+            <!-- Footer -->
+        </footer>
+
+    </div>
+</body>
+</html>
